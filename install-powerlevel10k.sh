46da999df7ff2947bd3360a1feb209983edd9730
Iris Li <github@iris.li>
2020-06-05 17:31:17 -0700

    Improve install script

--- install-powerlevel10k.sh
+++ install-powerlevel10k.sh
@@ -9,3 +9,10 @@ else
     echo "If it is not loading, please run  echo source ${P10K_DIR}/powerlevel10k.zsh-theme >>! ~/.zshrc"
 fi
 cp ~/environment/.p10k.zsh ~/.p10k.zsh
+touch ~/.zshrc
+if cat ~/.zshrc | grep "[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh" --quiet; then
+  echo '.p10k.zsh already installed in ~/.zshrc'
+else
+  printf "\n[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh\n" >> ~/.zshrc
+  echo 'Added line to ~/.zshrc'
+fi
