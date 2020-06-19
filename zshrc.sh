92f45c96b9c86924bc86715667391bc91a8fc1d3
Iris Li <github@iris.li>
2020-06-18 20:03:34 -0700

    Add cross-platform ls colors and autoformat

--- zshrc.sh
+++ zshrc.sh
@@ -15,114 +15,107 @@ set -o ignoreeof
 
 # Define some color codes so that we can easily use them
 # From https://wiki.archlinux.org/index.php/Color_Bash_Prompt
-    # Reset
-    Color_Off='\e[0m'       # Text Reset
+# Reset
+Color_Off='\e[0m' # Text Reset
 
-    # Regular Colors
-    Black='\e[0;30m'        # Black
-    Red='\e[0;31m'          # Red
-    Green='\e[0;32m'        # Green
-    Yellow='\e[0;33m'       # Yellow
-    Blue='\e[0;34m'         # Blue
-    Purple='\e[0;35m'       # Purple
-    Cyan='\e[0;36m'         # Cyan
-    White='\e[0;37m'        # White
+# Regular Colors
+Black='\e[0;30m'  # Black
+Red='\e[0;31m'    # Red
+Green='\e[0;32m'  # Green
+Yellow='\e[0;33m' # Yellow
+Blue='\e[0;34m'   # Blue
+Purple='\e[0;35m' # Purple
+Cyan='\e[0;36m'   # Cyan
+White='\e[0;37m'  # White
 
-    # Bold
-    BBlack='\e[1;30m'       # Black
-    BRed='\e[1;31m'         # Red
-    BGreen='\e[1;32m'       # Green
-    BYellow='\e[1;33m'      # Yellow
-    BBlue='\e[1;34m'        # Blue
-    BPurple='\e[1;35m'      # Purple
-    BCyan='\e[1;36m'        # Cyan
-    BWhite='\e[1;37m'       # White
+# Bold
+BBlack='\e[1;30m'  # Black
+BRed='\e[1;31m'    # Red
+BGreen='\e[1;32m'  # Green
+BYellow='\e[1;33m' # Yellow
+BBlue='\e[1;34m'   # Blue
+BPurple='\e[1;35m' # Purple
+BCyan='\e[1;36m'   # Cyan
+BWhite='\e[1;37m'  # White
 
-    # Underline
-    UBlack='\e[4;30m'       # Black
-    URed='\e[4;31m'         # Red
-    UGreen='\e[4;32m'       # Green
-    UYellow='\e[4;33m'      # Yellow
-    UBlue='\e[4;34m'        # Blue
-    UPurple='\e[4;35m'      # Purple
-    UCyan='\e[4;36m'        # Cyan
-    UWhite='\e[4;37m'       # White
+# Underline
+UBlack='\e[4;30m'  # Black
+URed='\e[4;31m'    # Red
+UGreen='\e[4;32m'  # Green
+UYellow='\e[4;33m' # Yellow
+UBlue='\e[4;34m'   # Blue
+UPurple='\e[4;35m' # Purple
+UCyan='\e[4;36m'   # Cyan
+UWhite='\e[4;37m'  # White
 
-    # Background
-    On_Black='\e[40m'       # Black
-    On_Red='\e[41m'         # Red
-    On_Green='\e[42m'       # Green
-    On_Yellow='\e[43m'      # Yellow
-    On_Blue='\e[44m'        # Blue
-    On_Purple='\e[45m'      # Purple
-    On_Cyan='\e[46m'        # Cyan
-    On_White='\e[47m'       # White
+# Background
+On_Black='\e[40m'  # Black
+On_Red='\e[41m'    # Red
+On_Green='\e[42m'  # Green
+On_Yellow='\e[43m' # Yellow
+On_Blue='\e[44m'   # Blue
+On_Purple='\e[45m' # Purple
+On_Cyan='\e[46m'   # Cyan
+On_White='\e[47m'  # White
 
-    # High Intensity
-    IBlack='\e[0;90m'       # Black
-    IRed='\e[0;91m'         # Red
-    IGreen='\e[0;92m'       # Green
-    IYellow='\e[0;93m'      # Yellow
-    IBlue='\e[0;94m'        # Blue
-    IPurple='\e[0;95m'      # Purple
-    ICyan='\e[0;96m'        # Cyan
-    IWhite='\e[0;97m'       # White
+# High Intensity
+IBlack='\e[0;90m'  # Black
+IRed='\e[0;91m'    # Red
+IGreen='\e[0;92m'  # Green
+IYellow='\e[0;93m' # Yellow
+IBlue='\e[0;94m'   # Blue
+IPurple='\e[0;95m' # Purple
+ICyan='\e[0;96m'   # Cyan
+IWhite='\e[0;97m'  # White
 
-    # Bold High Intensity
-    BIBlack='\e[1;90m'      # Black
-    BIRed='\e[1;91m'        # Red
-    BIGreen='\e[1;92m'      # Green
-    BIYellow='\e[1;93m'     # Yellow
-    BIBlue='\e[1;94m'       # Blue
-    BIPurple='\e[1;95m'     # Purple
-    BICyan='\e[1;96m'       # Cyan
-    BIWhite='\e[1;97m'      # White
+# Bold High Intensity
+BIBlack='\e[1;90m'  # Black
+BIRed='\e[1;91m'    # Red
+BIGreen='\e[1;92m'  # Green
+BIYellow='\e[1;93m' # Yellow
+BIBlue='\e[1;94m'   # Blue
+BIPurple='\e[1;95m' # Purple
+BICyan='\e[1;96m'   # Cyan
+BIWhite='\e[1;97m'  # White
 
-    # High Intensity backgrounds
-    On_IBlack='\e[0;100m'   # Black
-    On_IRed='\e[0;101m'     # Red
-    On_IGreen='\e[0;102m'   # Green
-    On_IYellow='\e[0;103m'  # Yellow
-    On_IBlue='\e[0;104m'    # Blue
-    On_IPurple='\e[0;105m'  # Purple
-    On_ICyan='\e[0;106m'    # Cyan
-    On_IWhite='\e[0;107m'   # White
-
+# High Intensity backgrounds
+On_IBlack='\e[0;100m'  # Black
+On_IRed='\e[0;101m'    # Red
+On_IGreen='\e[0;102m'  # Green
+On_IYellow='\e[0;103m' # Yellow
+On_IBlue='\e[0;104m'   # Blue
+On_IPurple='\e[0;105m' # Purple
+On_ICyan='\e[0;106m'   # Cyan
+On_IWhite='\e[0;107m'  # White
 
 # Takes the all the letters of whoami and turns it into a number between 0 and 5 so that we can uniquely color/style the username
 # Does so by summing up the decimal representation of all the numbers
 # and then getting the remainder when dividing by the amount of color options we have
 unique_string=$USER$(groups)
 unique_sum=$(echo $unique_string | sum | tr -d " ")
-unique_num=$(( $unique_sum  % 7 ))
+unique_num=$(($unique_sum % 7))
 
-if [ "$unique_num" -eq 0 ]
-then
+if [ "$unique_num" -eq 0 ]; then
     ucolor1=$Red
     ucolor2=$Yellow
     ucolor3=$Blue
-elif [ "$unique_num" -eq 1 ]
-then
+elif [ "$unique_num" -eq 1 ]; then
     ucolor1=$Yellow
     ucolor2=$Blue
     ucolor3=$Purple
-elif [ "$unique_num" -eq 2 ]
-then
+elif [ "$unique_num" -eq 2 ]; then
     ucolor1=$Blue
     ucolor2=$Purple
     ucolor3=$Green
-elif [ "$unique_num" -eq 3 ]
-then
+elif [ "$unique_num" -eq 3 ]; then
     ucolor1=$Purple
     ucolor2=$Green
     ucolor3=$Cyan
-elif [ "$unique_num" -eq 4 ]
-then
+elif [ "$unique_num" -eq 4 ]; then
     ucolor1=$Green
     ucolor2=$Cyan
     ucolor3=$Red
-elif [ "$unique_num" -eq 5 ]
-then
+elif [ "$unique_num" -eq 5 ]; then
     ucolor1=$Cyan
     ucolor2=$Red
     ucolor3=$Yellow
@@ -132,18 +125,21 @@ else # 6
     ucolor3=$Purple
 fi
 
-
-
 export LSCOLORS=ExFxCxDxBxegedabagacad
-# Check if ls uses the --color argument
+# Cross-platform ls colors
+if ls --help 2>&1 | grep -q -- --color; then
+    alias ls='ls --color=auto -F'
+else
+    alias ls='ls -FG'
+fi
+
 alias l="ls  -lGhpFtr" # Most recent first
-alias ll="ls -lGhpF" # "Vanilla" ll
-alias la="ls -lGhpFa" # "Vanilla" ll with hidden files
+alias ll="ls -lGhpF"   # "Vanilla" ll
+alias la="ls -lGhpFa"  # "Vanilla" ll with hidden files
 
 alias lg="l | grep"
 alias llg="ll | grep"
 alias lag="la | grep"
-
 
 # Moving up directories quicker
 alias ..="cd .."
@@ -152,9 +148,7 @@ alias ....="cd ../../.."
 alias .....="cd ../../../.."
 alias ......="cd ../../../../.."
 
-
 alias mkdirp="mkdir -p"
-
 
 # Some programs are missing on the berkeley eecs servers
 # We don't use $PATH because we need to check if each program is available
@@ -173,8 +167,6 @@ alias mkdirp="mkdir -p"
 #     # This is a 12.04 ubuntu binary
 #     alias htop="${ENVDIR}/bin/htop"
 # fi
-
-
 
 # BC Calculator default scale to show up to the satoshi (smallest unit of Bitcoin)
 export BC_ENV_ARGS=${ENVDIR}/bcrc
@@ -205,7 +197,7 @@ alias mr="source make && source run.sh;" # Only run if we compiled successfully
 alias m2="source make2.sh"
 alias r2="source run2.sh"
 alias 2mr="source make2.sh && source run2.sh;" # Only run if we compiled successfully
-alias mr2="source make.sh && source run2.sh;" # Only run if we compiled successfully
+alias mr2="source make.sh && source run2.sh;"  # Only run if we compiled successfully
 
 #More colors
 alias grep="grep --color=always"
@@ -214,21 +206,25 @@ export CLICOLOR=1
 # Get public ip
 alias ip="dig +short myip.opendns.com @resolver1.opendns.com"
 
-
 # Loops through folders in current directory
 forin() {
-  for dir in ./*; do
-    if [[ -d $dir ]]; then
-      (echo;echo "--------------------------------------------------------------------------------$dir"; cd "$dir";${*:1} )
-    fi
-  done
+    for dir in ./*; do
+        if [[ -d $dir ]]; then
+            (
+                echo
+                echo "--------------------------------------------------------------------------------$dir"
+                cd "$dir"
+                ${*:1}
+            )
+        fi
+    done
 }
 
 # git commamands simplified
 git config --global push.default current
-alias g='git status' # G
-alias gs='git status' # sorry, ghostscript
-alias gsr='forin git status' # git status recursive
+alias g='git status'               # G
+alias gs='git status'              # sorry, ghostscript
+alias gsr='forin git status'       # git status recursive
 alias gcoh='git checkout HEAD -- ' # hard reset a single file
 alias gco='git checkout'
 alias gcob='git checkout -b' # create branch
@@ -256,7 +252,6 @@ alias glg2='git log --date-order --all --graph --name-status --format="%C(green)
 # Ansible alias
 alias apb="ansible-playbook"
 
-
 # Update our environment by pulling the git and then returning back to where we were and refresh settings
 # Also updates git plugins
 
@@ -270,8 +265,7 @@ alias c='clear'
 #echo -e "${colorcode}                 ${NC}\n${colorcode}    $(whoami)    ${NC}\n${colorcode}                 ${NC}"
 
 for f in ${ENVDIR}/extra/*.sh; do
-    if [ $f = $ENVDIR'/extra/blank.sh' ];
-    then
+    if [ $f = $ENVDIR'/extra/blank.sh' ]; then
         : #nop
     else
         . $f
